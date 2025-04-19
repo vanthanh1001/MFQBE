1ab5f0f4fd7453cc1deff8a6c0712424fb1c5fcf
vanthanh1001 <thanhvtvn321@gmail.com>
2025-04-19 16:23:18 +0700

    Trainer

--- obj/Debug/net8.0/FitnessApp.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitnessApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c687e8f48b3ca68294ba985b92c2e2f3c2dbdd62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf67ba7250a35f94a5fa20049db1bdf6986ab09d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
